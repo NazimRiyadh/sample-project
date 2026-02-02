ae0e641686bd551d7b1586915dab3b81fcc4e91a
NazimRiyadh <neariyadh786@gmail.com>
2026-02-02 23:36:36 +0600

    DB CRUD queries added in db file

---
+++ backend/db_queries/0011-Insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO USERS(
+	first_name,
+	last_name,
+	email,
+	password
+) VALUES (
+	'Abdul',
+	'Azad',
+	'Azad@gmail.com',
+	'1234'
+)
---
+++ backend/db_queries/0013-update.sql
@@ -0,0 +1,4 @@
+UPDATE USERS  SET
+	first_name = 'Sadik' , last_name = 'Kaium'
+
+WHERE id=2
